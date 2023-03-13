55854289bef8504c40b19781744752d8940e77e2
nherrera18 <127697135+Nherrera18@users.noreply.github.com>
2023-03-12 20:45:46 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a special 'matrix' object that can chache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+##This function computes the inverse of the special 'matrix' returned by function above
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+makevector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+}
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function()m 
+        list(set = set, get = get,
+             setmean = setmean, 
+             getmean = getmean)}
